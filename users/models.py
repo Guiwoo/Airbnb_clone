cfdfacc6df779ea413fac112f9d986f442913eb4
Guiwoo <park.guiwoo@hotmail.com>
2021-11-17 15:35:59 +0900

    add more users model(birthday, language, currency, superhost)

--- users/models.py
+++ users/models.py
@@ -15,9 +15,26 @@ class User(AbstractUser):
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICE = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    LANGUAGE_CHOICE = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "한국어"))
 
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICE, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="")
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
