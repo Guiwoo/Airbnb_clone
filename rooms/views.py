b78d6b3b23c6715431eefc4c7ca8c5fbf3522f60
Guiwoo <park.guiwoo@hotmail.com>
2021-12-10 20:47:42 +0900

    detail pages

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import redirect, render
-from django.core.paginator import EmptyPage, Paginator
+from django.utils import timezone
+from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from . import models
 
 
-def all_rooms(req):
-    page = int(req.GET.get("page", 1))
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
+class HomeView(ListView):
+    """Homeview Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+
+def room_detail(requset, pk):
     try:
-        rooms = paginator.page(int(page))
-        return render(req, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+        room = models.Room.objects.get(pk=pk)
+        return render(requset, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
