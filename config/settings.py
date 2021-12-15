21fa4312a13c380398070976ebe33b1012158742
Guiwoo <park.guiwoo@hotmail.com>
2021-12-16 00:18:14 +0900

    email add

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = "/static/"
 AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = "/media/"
+
+# Email Configuration
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "noreply@sandbox527e4acdce0c45a885c435359f824b8e.mailgun.org"
