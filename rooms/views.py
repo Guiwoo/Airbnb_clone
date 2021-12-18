262804d89ebc3c45e4f2e7ebbfed4223f0884fcb
Guiwoo <park.guiwoo@hotmail.com>
2021-12-18 20:39:11 +0900

    room_card_done

--- rooms/views.py
+++ rooms/views.py
@@ -9,7 +9,7 @@ class HomeView(ListView):
     """Homeview Definition"""
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     ordering = "created"
     paginate_orphans = 5
     context_object_name = "rooms"
